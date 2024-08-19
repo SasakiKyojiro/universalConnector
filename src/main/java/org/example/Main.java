3cde503db1085840308910021360b30f38189267
SasakiKyojiro <fludilkaobyknovennaya@yandex.ru>
2024-08-19 20:22:38 +0300

    Добавил масштабируемую систему проверки json конфига.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import client.PackageProcessor;
+import config.inspector.AvailabilityInspector;
+import config.inspector.QualityInspector;
 import config.json.Config;
 import config.parser.ConfigParser;
 import log.LevelLog;
@@ -9,7 +11,6 @@ import log.LogUtil;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 import static log.LevelLog.*;
 
@@ -17,26 +18,33 @@ public class Main {
     public static void main(String[] args) {
         String configFilePath = "config.json";  // Путь к файлу конфигурации
         Config config = null;
+        boolean parsedItCorrectly = false;
 //        LogUtil logger = new LogUtil();
         try {
             System.out.println("Начат парсинг");
             ConfigParser configParser = new ConfigParser();
             config = configParser.parseConfig(configFilePath);
             if (config != null) {
-                System.out.println("Успешно завершён парсинг");
+                parsedItCorrectly = AvailabilityInspector.availabilityInspector(config);
+                if(parsedItCorrectly){
+                    parsedItCorrectly = QualityInspector.qualityInspector(config);
+                    if(!parsedItCorrectly){
+                        System.err.println("Ошибка в заполнении одного из пакетов в \"packages\"");
+                    }
+                }
 //                logger.log(config.getLog_path(), "Starting connector");
             }
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
-        if (config != null) {
+        if (config != null && parsedItCorrectly) {
             Map<String, LevelLog> logLevelMap = new HashMap<>();
             logLevelMap.put(Debug.toString(), Debug);
             logLevelMap.put(Error.toString(), Error);
             logLevelMap.put(Fatal.toString(), Fatal);
             logLevelMap.put(Warning.toString(), Warning);
 
-            LevelLog level = logLevelMap.getOrDefault(config.getLogLevel(), Fatal);
+            LevelLog level = logLevelMap.get(config.getLogLevel());
             LogUtil logUtil = new LogUtil(config.getLogPath(), level);
             logUtil.log(Debug, "Запуск подключений к сервисам.");
             PackageProcessor processor = new PackageProcessor(config, logUtil);
